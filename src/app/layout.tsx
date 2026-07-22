8cb57926f6b05934f85e3d35ea50c66d05f8857c
myaxyo <mrmuhammadiy@gmail.com>
2026-07-22 10:56:06 +0200

    yandex metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -155,6 +155,17 @@ export default function RootLayout({
         />
       </head>
       <body className="antialiased">
+        {/* Yandex.Metrika */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};m[i].l=1*new Date();for(var j=0;j<document.scripts.length;j++){if(document.scripts[j].src===r){return;}}k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})(window,document,'script','https://mc.yandex.ru/metrika/tag.js?id=110936145','ym');ym(110936145,'init',{ssr:true,webvisor:true,clickmap:true,ecommerce:"dataLayer",referrer:document.referrer,url:location.href,accurateTrackBounce:true,trackLinks:true});`,
+          }}
+        />
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/110936145" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
         <TelemetryDeck>
           {children}
         </TelemetryDeck>
